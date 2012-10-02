cefa8fa2237176775175fa43fac28e6ecedc4a23
krobertson <nomail@nojustno.com>
2012-10-02 14:45:53 -0600

    chars

--- uart/src/menu_handlers.h
+++ uart/src/menu_handlers.h
@@ -48,21 +48,23 @@ void blinkCaller(){
 
 void led_control_menu_handler(uint8_t input){
 	switch(input){
-		case 1:
+		case '1':
 			start_blink(1);
+			send_LED_control_menu();
 			break;
-		case 2:
+		case '2':
 			stop_blink(0);
+			send_LED_control_menu();
 			break;
-		case 3:
+		case '3':
 			send_LED_frequency_menu();
 			current_menu = 2;
 			break;
-		case 4:
+		case '4':
 			send_LED_duty_cycle_menu();
 			current_menu = 3;
 			break;
-		case 5:
+		case '5':
 			send_arm_peripheral_control_menu();
 			current_menu = 0;
 			break;
@@ -76,19 +78,23 @@ void slf(int mode){
 
 void LED_frequency_menu_handler(uint8_t input){
 	switch(input){
-		case 1:
+		case '1':
 			slf(1);
+			send_LED_frequency_menu();
 			break;
-		case 2:
+		case '2':
 			slf(2);
+			send_LED_frequency_menu();
 			break;
-		case 3:
+		case '3':
 			slf(3);
+			send_LED_frequency_menu();
 			break;
-		case 4:
+		case '4':
 			slf(4);
+			send_LED_frequency_menu();
 			break;
-		case 5:
+		case '5':
 			send_LED_control_menu();
 			current_menu = 1;
 			break;
@@ -101,22 +107,27 @@ void lsdc(uint32_t dc){
 
 void LED_duty_cycle_menu_handler(uint8_t input){
 	switch(input){
-		case 1:
+		case '1':
 			lsdc(10);
+			send_LED_duty_cycle_menu();
 			break;
-		case 2:
+		case '2':
 			lsdc(25);
+			send_LED_duty_cycle_menu();
 			break;
-		case 3:
+		case '3':
 			lsdc(50);
+			send_LED_duty_cycle_menu();
 			break;
-		case 4:
+		case '4':
 			lsdc(75);
+			send_LED_duty_cycle_menu();
 			break;
-		case 5:
+		case '5':
 			lsdc(90);
+			send_LED_duty_cycle_menu();
 			break;
-		case 6:
+		case '6':
 			send_LED_control_menu();
 			current_menu = 1;
 			break;
